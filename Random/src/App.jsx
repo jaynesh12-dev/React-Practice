1ac3cd47dac658b65d4f66055c1cfcb270d8a070
jaynesh12-dev <jayneshaepa12@gmail.com>
2026-03-03 12:48:19 +0530

    Folder created

---
+++ Random/src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <>
+    <div>
+     <h1> Hello World</h1>
+     <h2> This is my first React App</h2>
+     <p> I am learning React and it's awesome!</p>
+    </div>
+    </>
+  )
+}
+
+export default App
